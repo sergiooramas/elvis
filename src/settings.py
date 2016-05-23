ed8d9508b1594dba610f23bf10bd58f888cd1af0
Sergio Oramas Martin <Sergio@soramas.ca.upf.edu>
2016-05-23 15:09:43 +0200

    readme changes

--- src/settings.py
+++ src/settings.py
@@ -2,11 +2,8 @@ import os
 
 relative_path = os.getcwd()
 PATH = relative_path[:relative_path.rfind("/")]
-#DBPEDIA_PATH = PATH+"/dbpedia/"
-DBPEDIA_PATH = "../../KnowledgeExtraction/dbpedia/"
+DBPEDIA_PATH = PATH+"/dbpedia/"
 TEXTS_PATH = PATH+"/texts/"
 DBPEDIA_SPOTLIGHT_ENDPOINT = "http://spotlight.sztaki.hu:2222/rest/annotate"
-#BABELFY_KEY = ""
-#TAGME_KEY = ""
-BABELFY_KEY = "fd04a614-db0e-47a3-a6c6-451803a560ea"
-TAGME_KEY = "454388ab362a716acb8201077dc8377b"
+BABELFY_KEY = ""
+TAGME_KEY = ""
