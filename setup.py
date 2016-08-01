0fa75ea9eac981161eba4b7a95c326a0f4f9dc53
Andres Ferraro <andres.ferraro@upf.edu>
2016-08-01 13:15:54 +0200

    Update setup to work with new structure

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ setup(name='elvis',
       author_email='sergio DOT oramas AT upf DOT edu',
       license='MIT',
       description='Framework to homogenize and combine the output of different entity linking tools.',
-      package_dir={'elvis': 'src'},
       packages=['elvis'],
       install_requires=[
       ],
