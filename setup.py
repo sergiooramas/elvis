879951936a8d7fcfad66bfc82eae92d41cb5bd92
Sergio Oramas Martin <Sergio@mbp-de-sergio-2.attlocal.net>
2016-08-05 01:03:15 -0700

    update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='elvis',
-      version='0.1.0',
+      version='0.1.1',
       author='Sergio Oramas',
       author_email='sergio DOT oramas AT upf DOT edu',
       license='MIT',
