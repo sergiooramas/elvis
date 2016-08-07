8d6216f81d9fa597061452ee67c877043e380b07
Andres Ferraro <aferraro@dcodesoft.com>
2016-08-07 14:49:19 +0200

    Update version to 0.1.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='elvis',
-      version='0.1.1',
+      version='0.1.2',
       author='Sergio Oramas',
       author_email='sergio DOT oramas AT upf DOT edu',
       license='MIT',
